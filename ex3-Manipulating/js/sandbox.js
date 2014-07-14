223cef769bcf0a00d0ee51e3aba6ff321b856d10
Ashwani Bachchas <ashubuddy89@gmail.com>
2014-07-14 13:06:51 +0530

    Add ex-3 maniuplating

---
+++ ex3-Manipulating/js/sandbox.js
@@ -0,0 +1,20 @@
+//1. Add five new list items to the end of the unordered list #myList.
+for (i=0; i < 5; i++){
+  var listLenght = $("#myList li").length + 1;
+  $("#myList").append("<li>List Item "+listLenght+"</li>")
+}
+
+
+//2. Remove the odd list items
+$("li:odd").remove();
+
+
+//3. Add another h2 and another paragraph to the last div.module
+$("div.module:last").append('<h2>Heading</h2><p>A new Paragraph added</p>');
+
+
+//4. Add another option to the select element; give the option the value "Wednesday"
+$("select option:eq(2)").after('<option value="wednesday">Wednesday</option>');
+
+//5. Add a new div.module to the page after the last one; put a copy of one of the existing images inside of it.
+$("div.module:last").after('<div class="module"><h2>New image added<h2/>',$("img:first").clone(),'</div>');
