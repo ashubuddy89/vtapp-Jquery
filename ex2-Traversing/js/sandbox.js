ea1c04af2166d559a1286b5f1aa9b0bc9d8ca2c3
Ashwani Bachchas <ashubuddy89@gmail.com>
2014-07-14 12:11:08 +0530

    Add ex-2 Traversing

---
+++ ex2-Traversing/js/sandbox.js
@@ -0,0 +1,21 @@
+//1. Select all of the image elements on the page; log each image's alt attribute.
+$('img').each(function() {
+  var thisAttr = $(this).attr('alt'); 
+  console.log(thisAttr);
+});
+
+
+//2. Select the search input text box, then traverse up to the form and add a class to the form.
+$('input.input_text').closest("form#search").addClass('search_form');
+
+
+//3. Select the list item inside #myList that has a class of "current" and remove that class from it; add a class of "current" to the next list item.
+$('#myList li.current').removeClass('current').next().addClass('current');
+
+
+//4. Select the select element inside #specials; traverse your way to the submit button.
+$('#specials select').closest("form").find('input.input_submit');
+
+
+//5. Select the first list item in the #slideshow element; add the class "current" to it, and then add a class of "disabled" to its sibling elements.
+$('#slideshow li:first').addClass('current').siblings("li").addClass('disabled');
