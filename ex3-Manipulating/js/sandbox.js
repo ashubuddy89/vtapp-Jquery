d62a86444b238b429fdfb92f062dcaa91c5778bf
Ashwani Bachchas <ashubuddy89@gmail.com>
2014-07-14 14:54:12 +0530

    create elements separately to append them in last div:module

--- ex3-Manipulating/js/sandbox.js
+++ ex3-Manipulating/js/sandbox.js
@@ -1,13 +1,11 @@
 //1. Add five new list items to the end of the unordered list #myList.
-for (i=0; i < 5; i++){
+for (i = 0; i < 5; i++) {
   var listLenght = $("#myList li").length + 1;
-  $("#myList").append("<li>List Item "+listLenght+"</li>")
+  $("#myList").append("<li>List Item " + listLenght + "</li>")
 }
-
 
 //2. Remove the odd list items
 $("li:odd").remove();
-
 
 //3. Add another h2 and another paragraph to the last div.module
 var $newHeading = "<h2>Heading</h2>";
@@ -15,10 +13,8 @@ var $newParagraph = "<p>A new Paragraph added</p>";
 $("div.module:last").append($newHeading);
 $("div.module:last").append($newParagraph);
 
-
 //4. Add another option to the select element; give the option the value "Wednesday"
 $("select option:eq(2)").after('<option value="wednesday">Wednesday</option>');
-
 
 //5. Add a new div.module to the page after the last one; put a copy of one of the existing images inside of it.
 $("div.module:last").after('<div class="module"><h2>New image added<h2/>',$("img:first").clone(),'</div>');
