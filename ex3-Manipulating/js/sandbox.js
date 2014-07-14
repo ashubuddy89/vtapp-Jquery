2ab741ef7140c46ecf683c335703e7ba9cf6ecd9
Ashwani Bachchas <ashubuddy89@gmail.com>
2014-07-14 14:32:20 +0530

    creare elements separately to append them in last div:module

--- ex3-Manipulating/js/sandbox.js
+++ ex3-Manipulating/js/sandbox.js
@@ -10,11 +10,15 @@ $("li:odd").remove();
 
 
 //3. Add another h2 and another paragraph to the last div.module
-$("div.module:last").append('<h2>Heading</h2><p>A new Paragraph added</p>');
+var $newHeading = "<h2>Heading</h2>";
+var $newParagraph = "<p>A new Paragraph added</p>";
+$("div.module:last").append($newHeading);
+$("div.module:last").append($newParagraph);
 
 
 //4. Add another option to the select element; give the option the value "Wednesday"
 $("select option:eq(2)").after('<option value="wednesday">Wednesday</option>');
+
 
 //5. Add a new div.module to the page after the last one; put a copy of one of the existing images inside of it.
 $("div.module:last").after('<div class="module"><h2>New image added<h2/>',$("img:first").clone(),'</div>');
