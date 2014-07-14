91d648f1bc88c74f3b3cd2b1dbe8d967c89c613c
Ashwani Bachchas <ashubuddy89@gmail.com>
2014-07-14 17:29:56 +0530

    use nth-child to find the list item

--- ex1-Selecting/js/sandbox.js
+++ ex1-Selecting/js/sandbox.js
@@ -5,7 +5,7 @@ console.log($divClassModule = $('div.module'));
 var $myListItem;
 console.log("2. Come up with three selectors that you could use to get the third item in the #myList unordered list. Which is the best to use? Why?")
 console.log($myListItem = $('#myList li').eq(2));
-console.log($myListItem = $('#myListItem')); // it directly searches the elements.
+console.log($myListItem = $('#myList li:nth-child(3)')); // it directly searches the elements.
 console.log($myListItem = $('#myList').find("#myListItem"));
 
 var $searchLabel;
@@ -22,4 +22,4 @@ console.log($findImageWithAltAttr = $('img[alt]').length);
 
 var $tableOddRows;
 console.log("6. Select all of the odd table rows in the table body.")
-console.log($tableOddRows = $('tr:odd'));
+console.log($tableOddRows = $('table tr:odd'));
