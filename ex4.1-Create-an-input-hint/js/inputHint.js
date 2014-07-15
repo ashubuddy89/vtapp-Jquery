ed16ebbb303f23c94f0ba57be4a172f104aec450
Ashwani Bachchas <ashubuddy89@gmail.com>
2014-07-15 13:55:06 +0530

    optimize code and use chain system to call methods

--- ex4.1-Create-an-input-hint/js/inputHint.js
+++ ex4.1-Create-an-input-hint/js/inputHint.js
@@ -5,22 +5,25 @@ var InputHint = function (elemLabel, elemInput, hintClass) {
 }
 
 InputHint.prototype.setInputValue = function () {
-  var $labelText = $(this.elemLabel).text();
+  var $labelText = this.elemLabel.text();
   this.elemInput.val($labelText);
+  return this;
 }
 
 InputHint.prototype.addClassOnInput = function () {
   this.elemInput.addClass(this.hintClass);
+  return this;
 }
 
 InputHint.prototype.removeLabel = function () {
   this.elemLabel.remove();
+  return this;
 }
 
 InputHint.prototype.inputFocusEvent = function () {
   var _this = this;
   this.elemInput.bind("focus", function () {
-    $(this).removeClass(_this.hintClass).val("");
+    _this.elemInput.removeClass(_this.hintClass).val("");
   })
 }
 
@@ -28,7 +31,7 @@ InputHint.prototype.inputBlurEvent = function () {
   var _this = this;
 
   this.elemInput.bind("blur", function () {
-    if ($(this).val().trim() == "") {
+    if (_this.elemInput.val().trim() == "") {
       _this.setInputValue();
       _this.addClassOnInput();
     }
@@ -42,8 +45,8 @@ InputHint.prototype.bindInputEvents = function () {
 
 $(function () {
   var input_hint = new InputHint($("#search label"), $(".input_text"), "hint");
-  input_hint.setInputValue();
-  input_hint.addClassOnInput();
-  input_hint.removeLabel();
-  input_hint.bindInputEvents();
+  input_hint.setInputValue()
+            .addClassOnInput()
+            .removeLabel()
+            .bindInputEvents();
 })
