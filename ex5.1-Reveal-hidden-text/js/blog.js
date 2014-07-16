e60a8555d04c20619e66342aabbad8db5e2c30a9
Ashwani Bachchas <ashubuddy89@gmail.com>
2014-07-16 22:42:32 +0530

    use jQuery objects

--- ex5.1-Reveal-hidden-text/js/blog.js
+++ ex5.1-Reveal-hidden-text/js/blog.js
@@ -1,18 +1,18 @@
-var Accordion = function (elemLink, toggleElem) {
-  this.elemLink = $(elemLink);
-  this.toggleElem = $(toggleElem);
+var Accordion = function ($elemLink, $toggleElem) {
+  this.$elemLink = $elemLink;
+  this.$toggleElem = $toggleElem;
 }
 
 Accordion.prototype.bindAccordionEvent = function () {
   var _this = this;
-  this.elemLink.on("click", function(event) {
+  this.$elemLink.on("click", function(event) {
+    event.preventDefault();
     var $this = $(this),
       $parentElem = $this.closest("li");
-    $parentElem.siblings("li").removeClass("active").find(_this.toggleElem).slideUp();
-    $parentElem.addClass("active").find(_this.toggleElem).slideDown();
-    event.preventDefault();
+    $parentElem.siblings("li").removeClass("active").find(_this.$toggleElem).slideUp();
+    $parentElem.addClass("active").find(_this.$toggleElem).slideDown();
   })
-};
+}
 
 $(function() {
   var accordion = new Accordion($(".toggle_link"), $(".excerpt"));
