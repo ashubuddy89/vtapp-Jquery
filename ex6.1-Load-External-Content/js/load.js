e78a850cf9bde061da5175706cb61cf1c4a9f28b
Ashwani Bachchas <ashubuddy89@gmail.com>
2014-07-16 22:55:12 +0530

    add ex6.1 ajax load content

---
+++ ex6.1-Load-External-Content/js/load.js
@@ -0,0 +1,29 @@
+var LoadAjaxData = function ($elemLink) {
+  this.$elemLink = $elemLink;
+}
+
+
+LoadAjaxData.prototype.createTargetElement = function(){
+  var _this = this;
+  this.$elemLink.each(function(){
+    var $targetElem = $('<div class="data_load"></div>') 
+    $(this).data('target', $targetElem);
+    $(this).closest("h3").after($targetElem);
+  })
+}
+
+LoadAjaxData.prototype.bindLoadEvent = function () {
+  var _this = this;
+  this.$elemLink.on("click", function(event) {
+    var $this = $(this),
+        thisAttrDataPage = ;
+    event.preventDefault();
+    $this.data('target').load('data/blog.html '+thisAttrDataPage);
+  })
+}
+
+$(function() {
+  var load_ajax_data = new LoadAjaxData($("#blog h3 > a"));
+  load_ajax_data.createTargetElement();
+  load_ajax_data.bindLoadEvent();
+})
