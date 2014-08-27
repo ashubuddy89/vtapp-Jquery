5c9c0ec29a14c4bf1c8ec3b8981945b707cdeeca
Ashwani Bachchas <ashubuddy89@gmail.com>
2014-08-27 16:52:26 +0530

    modularized methods

--- ex-product-grid-json/js/product_grid.js
+++ ex-product-grid-json/js/product_grid.js
@@ -3,33 +3,39 @@ var ProductFilter = function(){
   this.$filterBox = $(".left-container .filter-box");
 }
 
-ProductFilter.prototype.createFilterProductListing = function(json, key){
-  this.$productContainer.append( "<li id='" + json + "' class='product-img' data-color = '"+ key.color + "' data-brand = '" + key.brand+ "' data-sold = '" + key.sold_out + "'><img src=images/"+key.url+" /></li>" );
+ProductFilter.prototype.createFilterProductListing = function(id, item){
+  this.$productContainer.append( "<li id='" + id + "' class='product-img' data-color = '"+ item.color + "' data-brand = '" + item.brand+ "' data-sold = '" + item.sold_out + "'><img src=images/"+item.url+" /></li>" );
 }
 
 ProductFilter.prototype.jsonData = function(){
   var _this = this;
   $.getJSON( "data/product.json", function( json ) {
-    $.each( json, function( json, key ) {
-      _this.createFilterProductListing(json, key);
+    $.each( json, function(index) {
+      _this.createFilterProductListing(index, this);
     });
   });  
 }
 
 ProductFilter.prototype.filterData = function(filterBox, filterElem){
+  var _this = this;
   $(filterBox).each(function(){
     var filterArray = [],
-        $currentFilter = $(this);
+        $currentFilter = $(this),
         checkedInput = $currentFilter.find("input[name='"+$currentFilter.attr("id")+"']:checked");
-
     if(checkedInput.length > 0){
-      checkedInput.each(function(){
-        filterArray.push("[data-"+$currentFilter.attr('id')+" = '"+$(this).attr('data-'+$currentFilter.attr("id"))+"']");
-      })
+      _this.storeDataArray(checkedInput, filterArray, $currentFilter);
+      
+      //join array
       filterElem = filterElem.filter(filterArray.join());
     }
   })
   return filterElem;
+}
+
+ProductFilter.prototype.storeDataArray = function(elem, array, thisElem){
+  elem.each(function(){
+    array.push("[data-"+thisElem.attr('id')+" = '"+$(this).attr('data-'+thisElem.attr("id"))+"']");
+  });
 }
 
 ProductFilter.prototype.bindFilterClickEvent = function(){
