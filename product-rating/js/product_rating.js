ff6d85ec9d9e12ac49c24f0e6337bca72c455c3e
Ashwani Bachchas <ashubuddy89@gmail.com>
2014-10-03 16:10:15 +0530

    avoid trigger click and create seprate method for For rowTitleClickEvent and headingTitleClickEvent

--- product-rating/js/product_rating.js
+++ product-rating/js/product_rating.js
@@ -1,36 +1,37 @@
 var ProductRate = function () {
   this.products = ['Coffee', 'Tea', 'Sodas'];
-  this.likeOptions = ['Love it','Like it','No Views','Dislike it','Abhor it'];
+  this.likeOptions = ['Love it','Like it','No Views','Dislike it','Abhor it',];
   this.ratingContainer = $("#container");
   this.headingRow = $('<li class="heading" />');
 }
 
+
 ProductRate.prototype.createHeadingRow = function () {
   var _this = this;
-  $(this.ratingContainer).append(this.headingRow);
+  this.ratingContainer.append(this.headingRow);
 
   $.each(this.likeOptions, function (index) {
-    $(_this.headingRow).append($('<strong/>').text(this).addClass("col heading-title").attr("id", "prod" + index))
+    $(_this.headingRow).append($('<strong/>').text(this).addClass("col heading-title").attr("id",this.replace(/\s+/g, '-')))
   });
 }
 
-ProductRate.prototype.createRow = function (rowTitle, rowId) {
-  return $('<li class="rating-row" id="row' + rowId + '"><strong class="col title">' + rowTitle + '</strong></li>');
+ProductRate.prototype.createRow = function (rowTitle) {
+  return $('<li class="rating-row"><strong class="col title" id="' + rowTitle + '">' + rowTitle + '</strong></li>');
 }
 
-ProductRate.prototype.createButton = function (name, ratingAttr, rowAttr) {
-  return $('<label class="col radio-block"><input class="radio-btn" type="radio" name="' + name + '" data-rating="prod' + ratingAttr + '" data-row="row' + rowAttr + '"/></label>')
+ProductRate.prototype.createButton = function (name, ratingAttr) {
+  return $('<label class="col radio-block"><input class="radio-btn" type="radio" name="' + name + '" data-rating="' + ratingAttr + '" data-product="' + name + '"/></label>')
 }
 
 ProductRate.prototype.createProductRatingRow = function () {
   var _this = this;
   $.each(this.products, function (index) {
-    var row = _this.createRow(this, index);
+    var row = _this.createRow(this, this);
     $(_this.ratingContainer).append(row);
 
     //for radio button
     $.each(_this.likeOptions, function (index2) {
-      row.append(_this.createButton(_this.products[index], index2, index));
+      row.append(_this.createButton(_this.products[index], this.replace(/\s+/g, '-')));
     })
   });
 }
@@ -43,27 +44,25 @@ ProductRate.prototype.init = function ()  {
 ProductRate.prototype.radioBtnClickEvent = function () {
   $(".radio-btn").on("click", function(){
     var $this = $(this),
-        attrRating = $this.data("rating"),
-        attrRow = $this.data("row");
-    
-    $(".heading-title.active, .rating-row.active").removeClass("active");
-    $("#"+attrRating).addClass("active");
-    $("#"+attrRow).addClass("active");
+        ratingAttr = $this.data("rating"),
+        porducAttr = $this.data("product");    
+        
+    $(".col.active").removeClass("active");
+    $("#"+ratingAttr + ", #" + porducAttr).addClass("active");
   });
 }
+
 
 ProductRate.prototype.rowTitleClickEvent = function () {
   $(".rating-row .col.title").on("click", function () {
     var $this = $(this),
-        $parentElem  = $this.parent(".rating-row");
-        checkedRadio = $parentElem.find(".radio-btn:checked"),
-        checkedLength = $(checkedRadio).length;
-
-    if(checkedLength){
-      $(checkedRadio).trigger('click');
-    }else{
-      $parentElem.addClass("active").siblings(".rating-row").removeClass("active");
-      $(".heading-title").filter(".active").removeClass("active");
+        $parentElem  = $this.parent(".rating-row"),
+        checkedRadio = $parentElem.find(".radio-btn:checked");
+    
+    $(".title.active, .heading-title.active").removeClass("active");
+    $this.addClass("active");
+    if(checkedRadio.length){
+        $(".heading-title#"+checkedRadio.data("rating")).addClass("active")
     }
   });
 }
@@ -71,10 +70,11 @@ ProductRate.prototype.rowTitleClickEvent = function () {
 ProductRate.prototype.headingTitleClickEvent = function () {
   $(".heading-title").on("click" , function(){
     var $this = $(this),
-        $thisId = $this.attr("id"),
-        $activeRow = $(".rating-row.active");
-    if($activeRow.length){
-      $activeRow.find(".radio-btn[data-rating=" + $thisId + "]").trigger('click');  
+        $activeProduct = $(".rating-row .active");
+
+    if($activeProduct.length){
+      $this.addClass("active").siblings(".active").removeClass("active");
+      $activeProduct.parent(".rating-row").find(".radio-btn[data-rating=" + $this.attr("id") + "]").prop("checked", true);
     }
   });
 }
